ae965ab9c70cd20df4e96e58907e345d471fd64f
Kenneth <kenneth06201003@gmail.com>
2016-02-27 22:49:25 +0800

    animation

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,5 +1,8 @@
 (function(){
 	var app = angular.module('mainApp', ['ngMaterial']);
+	app.controller('indexCtrl', ['$scope', '$http', function ($scope, $http) {
+
+	}])	
 	app.controller('profileCtrl', ['$scope', '$http', function ($scope, $http) {
 		$http({method:'GET', url: '../data/profile.json'}).then(function success(data){
 			$scope.profile = data;
@@ -12,6 +15,15 @@
 		$http({method:'GET', url: '../data/portfolio.json'}).then(function success(data){
 			$scope.work = data.data.ch.work;
 			$scope.practice = data.data.ch.practice;
+		},
+		function fail(err){
+			console.log(err);
+		})		
+	}])	
+	app.controller('headerCtrl', ['$scope', '$http', function ($scope, $http) {
+		$scope.location =  window.location.pathname;
+		$http({method:'GET', url: '../data/nav.json'}).then(function success(data){
+			$scope.about = data.data.ch.about;
 		},
 		function fail(err){
 			console.log(err);
